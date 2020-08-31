f8bd05eba668ada3a5ae51189f16bec0b001f780
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-08-31 21:28:27 +0400

    requirements

--- link.py
+++ link.py
@@ -1,4 +1,4 @@
 import shortuuid
 def generateLink():
     randomGen = shortuuid.ShortUUID().random(length=7)
-    
+    return randomGen
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from db import col
+import os
+from db import col as mongoCol
+from link import generateLink
 import json
-from flask import Flask, request, Response
+from flask import Flask, request, Response, redirect
 from flask_api import status
+# app = Flask(__name__, static_folder='./frontend/build', static_url_path='/')
 app = Flask(__name__, static_folder='./frontend/build', static_url_path='/')
 
 
@@ -15,12 +18,27 @@ def create():
     if request.method == 'POST':
         req = request.get_json()
         generatedLink = generateLink()
-        response =
+        mongoCol.insert_one(
+            {"link": req.get('linkToRedirect'), "postfix": generatedLink})
+        response = Response(json.dumps(
+            {'createdLink': generatedLink}), status=status.HTTP_200_OK, mimetype="application/json")
     else:
         response = Response('Cannot get /create')
     return response
 
+@app.route('/<postfix>', methods=['GET'])
+def render_redirect_page(postfix):
+    return app.send_static_file('index.html')
+
+@app.route('/ls/<postfix>', methods=['GET'])
+def postfix(postfix):
+    el = mongoCol.find_one({"postfix": postfix})
+    if el:
+        return {'url': el["link"]}
+    else:
+        return Response('404', status=status.HTTP_404_NOT_FOUND)
+
 
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0', debug=False,
-                  port=os.environ.get("PORT", 8080))
+    app.run(debug=True,
+            port=os.environ.get("PORT", 8080))
