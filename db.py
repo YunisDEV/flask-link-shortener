70d3ca8dd3254c9bea5d3b7770f7b154fed35b87
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-09-01 12:24:12 +0400

    db

--- db.py
+++ db.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 import os
 
-client = MongoClient(os.environ.get("LS_DATABASE", "mongodb://localhost:27017/"))
+client = MongoClient(os.environ.get(
+    "LS_DATABASE", "mongodb://localhost:27017/"))
 
 db = client['link_shortener']
 
