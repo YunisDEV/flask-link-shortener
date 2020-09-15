b1640aabe98029e012e6d87f67e104420d206d0c
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-09-15 22:33:14 +0400

    sqlite integration

--- db.py
+++ db.py
@@ -1,9 +1,46 @@
-from pymongo import MongoClient
+import sqlite3
+import re
 import os
 
-client = MongoClient(os.environ.get(
-    "LS_DATABASE", "mongodb://localhost:27017/"))
+dbname = os.environ.get('LINK_SHORTENER_DBNAME')
 
-db = client['link_shortener']
+with sqlite3.connect(dbname) as conn:
+    c = conn.cursor()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS links(
+            id integer primary key autoincrement unique,
+            link text not null,
+            postfix varchar(10) not null unique
+        )
+        """)
 
-col = db['links']
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+def addLink(linkToRedirect, generatedLink):
+    with sqlite3.connect(dbname) as conn:
+        conn.row_factory = dict_factory
+        c = conn.cursor()
+        if not re.match('(?:http|ftp|https)://', linkToRedirect):
+            linkToRedirect = f'http://{linkToRedirect}'
+        c.execute(
+            f"INSERT INTO links(link,postfix) values('{linkToRedirect}','{generatedLink}')")
+        conn.commit()
+
+
+def getLink(postfix):
+    with sqlite3.connect(dbname) as conn:
+        conn.row_factory = dict_factory
+        c = conn.cursor()
+        c.execute(f"SELECT * FROM links WHERE postfix='{postfix}'")
+        data = c.fetchone()
+        if data:
+            fetchedLink = data.get('link')
+        else:
+            fetchedLink = None
+        return fetchedLink
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
-from db import col as mongoCol
 from link import generateLink
 import json
 from flask import Flask, request, Response, redirect
 from flask_api import status
-app = Flask(__name__, static_folder='./public', static_url_path='/')
+from db import addLink, getLink
 
+app = Flask(__name__, static_folder='./public', static_url_path='/')
 
 @app.route('/')
 def index():
@@ -17,8 +17,7 @@ def create():
     if request.method == 'POST':
         req = request.get_json()
         generatedLink = generateLink()
-        mongoCol.insert_one(
-            {"link": req.get('linkToRedirect'), "postfix": generatedLink})
+        addLink(req.get('linkToRedirect'),generatedLink)
         response = Response(json.dumps(
             {'createdLink': generatedLink}), status=status.HTTP_200_OK, mimetype="application/json")
     else:
@@ -33,8 +32,8 @@ def render_redirect_page(postfix):
 
 @app.route('/ls/<postfix>', methods=['GET'])
 def postfix(postfix):
-    el = mongoCol.find_one({"postfix": postfix})
-    if el:
-        return {'url': el["link"]}
+    fetchedLink = getLink(postfix)
+    if fetchedLink:
+        return {'url': fetchedLink}
     else:
         return Response('404', status=status.HTTP_404_NOT_FOUND)
